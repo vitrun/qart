aa3170d668cd884f040b96af7f47e922e341e93d
Alexandre Viau <alexandre@alexandreviau.net>
2016-05-14 13:42:32 -0400

    Added missing license

--- gf256/gf256.go
+++ gf256/gf256.go
@@ -1,6 +1,6 @@
 // Copyright 2010 The Go Authors.  All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE.bsd file.
 
 // Package gf256 implements arithmetic over the Galois Field GF(256).
 package gf256
